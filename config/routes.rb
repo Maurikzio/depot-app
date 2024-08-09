b5afcd8fe48554a031fac705b6397130dd44439c
Mauricio.M <m.morocho1196@gmail.com>
2024-08-09 17:16:49 -0500

    Make Catalog Listing page root

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  get 'store/index' #-> created by generate
-  # root "posts#index"
+  # get 'store/index' #-> created by generate
+  root "store#index", as: 'store_index'
 end
